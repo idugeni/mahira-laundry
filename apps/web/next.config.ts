4248ebf3c8b604300a5ab7104a925bc2ad79a210
Idugeni <officialelsa21@gmail.com>
2026-04-16 11:56:10 +0700

    fix: add i.pravatar.cc and ui-avatars.com to next/image remotePatterns

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -14,6 +14,16 @@ const nextConfig: NextConfig = {
 				hostname: "images.unsplash.com",
 				pathname: "/**",
 			},
+			{
+				protocol: "https",
+				hostname: "i.pravatar.cc",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "ui-avatars.com",
+				pathname: "/**",
+			},
 		],
 	},
 	env: {
